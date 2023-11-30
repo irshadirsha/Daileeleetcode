e5935924cd9aa78f0632b485684d3bc27869a90a
Mahammad Irshad ap <122246757+irshadirsha@users.noreply.github.com>
2023-11-30 14:09:57 +0530

    Time: 199 ms (5.01%) | Memory: 83.3 MB (5.30%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -5,14 +5,16 @@
  */
 var isAnagram = function(st1,st2) {
     console.log(st1,st2);
-    const s=[...st1].sort()
-    const t=[...st2].sort()
+    const s= mergeSort([...st1])
+    const t= mergeSort([...st2])
     console.log(s);
     console.log(t);
     let flag=0
-    if(s.length!=t.length){
-        return false
+    
+    if (s.length !== t.length) {
+        return false;
     }
+
     for(let i=0;i<s.length;i++){
         
             if(s[i]!==t[i]){
@@ -23,12 +25,42 @@ var isAnagram = function(st1,st2) {
     
 
     if (flag==0) {
-        return true
+       return true
     } else {
-       return false
+        return false
     }
     
 };
+
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
 
 
 
