179be508fac45c569712a2ab089c13fffe44af0a
Mahammad Irshad ap <122246757+irshadirsha@users.noreply.github.com>
2023-12-02 15:02:15 +0530

    Time: 54 ms (39.72%) | Memory: 41.6 MB (79.42%) - LeetSync

--- 66-plus-one/plus-one.js
+++ 66-plus-one/plus-one.js
@@ -3,16 +3,11 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    console.log(digits)
     let arr=[]
     let number=digits.join('')
-    console.log(number);
     let numbers=BigInt(number)
-    console.log(numbers);
      arr.push(numbers+1n)
     const num = arr[0].toString().split('').map(Number)
-     console.log(arr);
-     console.log(num);
      return num
 
 };
