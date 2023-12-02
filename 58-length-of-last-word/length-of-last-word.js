d4a6de7d49eec1c8b626709c6e623e477a12718e
Mahammad Irshad ap <122246757+irshadirsha@users.noreply.github.com>
2023-12-02 13:30:20 +0530

    Time: 52 ms (48.96%) | Memory: 44.1 MB (5.11%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    const words = s.split(' ');
+    let newarr=[]
+    let chararr=[]
+    let flag=0;
+      console.log("newone",words)
+      for(let i=words.length-1;i>=0;i--){
+        if(words[i]!==""){
+            newarr.push(words[i])
+            break;
+        }else{
+            continue
+        }
+      }
+      let len=newarr[0].split('').length
+      console.log(newarr)
+      console.log(len)
+      return len
+
+       
+};
